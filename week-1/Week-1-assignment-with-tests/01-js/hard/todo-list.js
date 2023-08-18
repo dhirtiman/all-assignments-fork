79a15a3a1ce6104356ed5a6bce68925887b63f02
dhirtiman <dhirtimankhati@gmail.com>
2023-08-18 20:18:19 +0530

    week1

--- week-1/Week-1-assignment-with-tests/01-js/hard/todo-list.js
+++ week-1/Week-1-assignment-with-tests/01-js/hard/todo-list.js
@@ -12,6 +12,39 @@
 */
 
 class Todo {
+  constructor(){
+    this.list = [];
+  }
+
+  add(todo){
+    this.list.push(todo);
+  }
+
+  remove(index){
+    this.list.splice(index,1);
+  }
+
+  update(index,updatedTodo){
+    if(index < this.list.length){
+      this.list[index] = updatedTodo;
+    }
+  }
+
+  getAll(){
+    return this.list;
+  }
+
+  get(index){
+    if(index < this.list.length){
+      return this.list[index];
+    }else {
+      return null;
+    }
+  }
+
+  clear(){
+    this.list = [];
+  }
 
 }
 
